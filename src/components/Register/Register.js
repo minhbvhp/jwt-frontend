eaf2fe0674f3a995c485f3ff7a27497557d7be5b
trankhoaminh <tranminhbvhp@gmail.com>
2024-03-29 10:58:43 +0700

    10.2.2

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import { registerNewUser } from '../../services/userService';
 
 const Register = (props) => {
     const [email, setEmail] = useState("");
@@ -83,13 +84,19 @@ const Register = (props) => {
         return true;
     }
 
-    const handleRegister = () => {
+    const handleRegister = async () => {
         let check = isValidInputs();
 
         if (check === true) {
-            axios.post("http://localhost:8080/api/v1/register", {
-                email, phone, username, password
-            })
+            let response = await registerNewUser(email, phone, username, password);
+            let serverData = response.data;
+
+            if (+serverData.EC === 0) {
+                toast.success(serverData.EM);
+                history.push("/login");
+            } else {
+                toast.error(serverData.EM);
+            }
         }
 
     }
